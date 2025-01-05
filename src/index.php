1f341452113f6a0d1e9eea1bc007a0eb84529548
Itay Raz <itay@studioraz.co.il>
2025-01-05 15:16:12 +0200

    include localhost + delete kiil cron task

--- src/index.php
+++ src/index.php
@@ -3,8 +3,17 @@
 namespace StudioRaz\DeployerExtraTasks;
 
 require __DIR__ . '/Deployer/constants.php';
+
+/** localhost */
+require __DIR__ . '/Deployer/dev/localhost-env.php';
+require __DIR__ . '/Deployer/dev/build-static.php';
+
+/** magento */
 require __DIR__ . '/Deployer/magento/set_cache_prefix.php';
 require __DIR__ . '/Deployer/magento/cleanup_cache_prefix.php';
-require __DIR__ . '/Deployer/studioraz/cron_kill.php';
+
+/** studioraz */
 require __DIR__ . '/Deployer/studioraz/hyva_build_tailwind.php';
+
+/** deployer  */
 require __DIR__ . '/Deployer/deploy/vendors.php';
